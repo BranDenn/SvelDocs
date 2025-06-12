0fb6bd6ca10f0a9f27b37509956de005c67db4ce
BranDenn <76827979+BranDenn@users.noreply.github.com>
2025-06-11 22:00:47 -0700

    flexsearch import.meta fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	ssr: {
+		noExternal: ['flexsearch'] // This fixes following error "Cannot use 'import.meta' outside a module" that may occur on some computers
+	},
 	server: {
 		fs: {
 			allow: ['doc.config.ts', 'mdsvex.config.js']
