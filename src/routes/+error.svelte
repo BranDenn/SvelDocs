9f656c1f8c4ab0f2da6ecc97cca884980ba2c86e
BranDenn <76827979+BranDenn@users.noreply.github.com>
2025-08-04 18:04:19 -0700

    format

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,10 +6,13 @@
 	<title>Error {page.status} - SvelDocs</title>
 </svelte:head>
 
-<div class="grow grid content-center p-4 justify-items-center gap-8">
+<div class="grid grow content-center justify-items-center gap-8 p-4">
 	<div class="text-center">
 		<h1 class="text-2xl">Error {page.status}</h1>
 		{page.error?.message}
 	</div>
-	<a class="border rounded py-1 font-medium px-2 hover:bg-foreground transition-colors hover:text-primary text-secondary hover:border-accent" href='/'>Home</a>
+	<a
+		class="hover:bg-foreground hover:text-primary text-secondary hover:border-accent rounded border px-2 py-1 font-medium transition-colors"
+		href="/">Home</a
+	>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
-<div class="grow grid content-center p-4 justify-items-center gap-2">
-    <h1 class="text-2xl">Welcome to <span class="text-accent">SvelDocs</span>.</h1>
-    <a class="border rounded py-1 font-medium px-2 hover:bg-foreground transition-colors hover:text-primary text-secondary hover:border-accent" href='/docs'>View Docs</a>
+<div class="grid grow content-center justify-items-center gap-2 p-4">
+	<h1 class="text-2xl">Welcome to <span class="text-accent">SvelDocs</span>.</h1>
+	<a
+		class="hover:bg-foreground hover:text-primary text-secondary hover:border-accent rounded border px-2 py-1 font-medium transition-colors"
+		href="/docs">View Docs</a
+	>
 </div>
