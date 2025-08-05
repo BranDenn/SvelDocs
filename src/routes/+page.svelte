9536753217e38b6bec495bf4f5d3af4fc621096f
BranDenn <76827979+BranDenn@users.noreply.github.com>
2025-08-04 18:09:06 -0700

    trying resolve for base path

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
 <div class="grid grow content-center justify-items-center gap-2 p-4">
 	<h1 class="text-2xl">Welcome to <span class="text-accent">SvelDocs</span>.</h1>
 	<a
 		class="hover:bg-foreground hover:text-primary text-secondary hover:border-accent rounded border px-2 py-1 font-medium transition-colors"
-		href="/docs">View Docs</a
+		href={resolve("/docs")}>View Docs</a
 	>
 </div>
